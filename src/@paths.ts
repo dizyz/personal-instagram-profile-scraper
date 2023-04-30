20bfcca3d89649b4c67dc469ff9a6885642275c5
Dizy <dev@dizy.cc>
2023-04-30 01:53:29 -0700

    Finish the script and automation

---
+++ src/@paths.ts
@@ -0,0 +1,6 @@
+import * as Path from "path";
+
+export const PROJECT_DIR = Path.resolve(__dirname, "..");
+
+export const PUBLIC_DIR = Path.resolve(PROJECT_DIR, "public");
+export const INSTAGRAM_JSON_PATH = Path.resolve(PUBLIC_DIR, `instagram.json`);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,34 @@
-import Puppeteer from 'puppeteer';
+import Puppeteer from "puppeteer";
+import * as FS from "fs-extra";
+import { INSTAGRAM_JSON_PATH, PUBLIC_DIR } from "./@paths";
 
-const INSTGRAM_URL = 'https://www.instagram.com/ziyang.io/';
+const INSTAGRAM_URL = "https://www.instagram.com/ziyang.io/";
 
+interface InstagramPhoto {
+  url: string;
+  caption: string;
+}
+
+(async () => {
+  // Prepare public folder if not exists
+  await FS.ensureDir(PUBLIC_DIR);
+
+  const browser = await Puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(INSTAGRAM_URL, { waitUntil: "networkidle2" });
+  await page.waitForSelector("article img", { visible: true });
+  const imageEls = await page.$$("article img");
+  const photoUrls: InstagramPhoto[] = await Promise.all(
+    imageEls.map(
+      async (el) => await el.evaluate((el) => el.getAttribute("src"))
+    )
+  );
+  await FS.writeJSON(
+    INSTAGRAM_JSON_PATH,
+    {
+      photos: photoUrls,
+    },
+    { spaces: 2 }
+  );
+  await browser.close();
+})().catch(console.error);
